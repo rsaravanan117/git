453c1b884e0e5ffbe6bda19ee6efc99a84e806e0
rsaravanan117 <142971075+rsaravanan117@users.noreply.github.com>
2023-09-08 12:05:06 +0530

    initial commit

---
+++ t1.py
@@ -0,0 +1,28 @@
+name = 'saravanan'
+age = 40
+print(name+' is a man')
+print(name+' is more practical')
+print(name, ' age is' ,age)  
+print(name.upper())
+print(name.lower().islower())
+print(name.islower())
+print(name.isupper())
+print(len(name))
+print(name.index('v'))
+print(name.replace('a','A'))
+
+from math import *
+number = 77
+number2 = str(number)
+print(' The number is '+number2)
+print(10*10, 10+10, 10/10, 100%9, abs(-59))
+from math import *
+print(sqrt(121))
+#name3 = input('Enter your name :')
+#age3 = int(input('Enter your age :'))
+#print(name3 ,'age is ',age3)    
+
+sentence = input('Enter your sentence :')
+word1= input('Enter the word to replace :') 
+word2= input('Enter the new word :') 
+print('new sentence is '+ sentence.replace(word1, word2)) 
